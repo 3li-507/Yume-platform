62989e663e552dbedfb099d2cae4de71e2ffc8b8
Saleh <salehalswat2@gmail.com>
2026-05-13 11:44:51 +0300

    Update base file & create contact

--- YumeProject/main/views.py
+++ YumeProject/main/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpRequest
+from django.contrib import messages
+from .models import Contact
 # Create your views here.
 
 def home_view(request: HttpRequest):
@@ -9,13 +11,35 @@ def home_view(request: HttpRequest):
 def about_view(request: HttpRequest):
     return render(request, 'main/about.html')
 
-def contact_view(request: HttpRequest):
+def contact_view(request:HttpRequest):
+    if request.method == 'POST':
+        Contact.objects.create(
+            first_name=request.POST.get('first_name'),
+            last_name=request.POST.get('last_name'),
+            email=request.POST.get('email'),
+            subject=request.POST.get('subject'),
+            message=request.POST.get('message'),
+        )
+        
+        messages.success(request, "Your message has been sent successfully!", "alert-success")
+
+        return redirect('main:home_view')
+
+
     return render(request, 'main/contact.html')
+
+def contact_view_all(request: HttpRequest):
+    messages = Contact.objects.all().order_by('-created_at')
+    return render(request, 'main/contact_view.html', {'messages': messages})
+
+def contact_view_detail(request: HttpRequest, contact_id):
+    message_detail = get_object_or_404(Contact, id=contact_id)
+    return render(request, 'main/contact_details.html', {'message': message_detail})
 
 def services_view(request: HttpRequest):
     return render(request, 'main/services.html')
 
-def Cities_view(request: HttpRequest):
+def cities_view(request: HttpRequest):
     return render(request, 'main/cities.html')
 
 
