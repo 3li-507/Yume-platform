fdf3599cc07a43942730a1aeb067ec70257fed81
M7mdQash <sil.qash@protonmail.com>
2026-05-13 09:27:37 +0300

    account backend fixing

--- YumeProject/accounts/forms.py
+++ YumeProject/accounts/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import RegexValidator
+
+
+
+phone_validator = RegexValidator(r'^\+?1?\d{9,15}$', 'Enter a valid phone number.')
 
 User = get_user_model()
 
@@ -10,6 +15,8 @@ class CustomerSignUpForm(UserCreationForm):
     last_name  = forms.CharField(max_length=150, required=True)
     email      = forms.EmailField(required=True)
     avatar     = forms.ImageField(required=False)
+    phone_number      = forms.CharField(required=True,validators=[phone_validator])
+   
 
     class Meta:
         model  = User
@@ -23,6 +30,7 @@ class OwnerSignUpForm(UserCreationForm):
     company_name = forms.CharField(max_length=200)
     company_id   = forms.CharField(max_length=100)
     avatar       = forms.ImageField(required=False)
+    phone_number = forms.CharField(required=True,validators=[phone_validator])
 
     class Meta:
         model  = User
--- YumeProject/accounts/models.py
+++ YumeProject/accounts/models.py
@@ -35,6 +35,7 @@ class User(AbstractUser):
 class CustomerProfile(models.Model):
     user   = models.OneToOneField('accounts.User', on_delete=models.CASCADE, related_name='customer_profile')
     avatar = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+    phone_number = models.CharField('Phone Number', blank=False,max_length=12)
 
     def __str__(self):
         return f"CustomerProfile({self.user.username})"
@@ -45,6 +46,10 @@ class OwnerProfile(models.Model):
     company_name = models.CharField(max_length=200)
     company_id   = models.CharField(max_length=100)
     avatar       = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+    phone_number = models.CharField('Phone Number', blank=False,max_length=12)
+    join_date    = models.DateTimeField(auto_now_add=True,blank=True, null=True)
+
+
 
     def __str__(self):
         return f"OwnerProfile({self.user.username} — {self.company_name})"
