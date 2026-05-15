66917d21ff241faf2b2420ef8a990bc98e367a55
M7mdQash <sil.qash@protonmail.com>
2026-05-15 20:38:06 +0300

    added dashboard view, waiting to change accounts.html

--- YumeProject/administration/views.py
+++ YumeProject/administration/views.py
@@ -1,7 +1,21 @@
 from django.http import HttpResponse
 from accounts.decorators import admin_role_required
+from django.shortcuts import render
+from hotels.models import CapsuleHotel, Capsule
+
+# Create your views here.
 
 
 @admin_role_required
 def admin_view(request):
-    return HttpResponse("Administration panel — coming soon.")
+    context ={
+        #'pending_hotels': CapsuleHotel.objects.filter(status=CapsuleHotel.STATUS_PENDING).select_related('hotel_owner__user', 'city'),
+#        'approved_hotels': CapsuleHotel.objects.filter(status=CapsuleHotel.STATUS_APPROVED).select_related('hotel_owner__user', 'city'),
+
+        'pending_hotels': CapsuleHotel.objects.select_related('hotel_owner__user', 'city'),
+        
+
+    }
+    return render(request, 'administration/dashboard.html')
+
+
